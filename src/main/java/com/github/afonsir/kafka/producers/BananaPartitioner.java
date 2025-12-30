5a2e49afb6e6009cd1bc31402b9a238675489561
AfonSir <afonsir17@gmail.com>
2025-12-30 19:29:43 -0300

    feat: add custom partitioner

---
+++ src/main/java/com/github/afonsir/kafka/producers/BananaPartitioner.java
@@ -0,0 +1,35 @@
+package com.github.afonsir.kafka.producers;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.kafka.clients.producer.Partitioner;
+import org.apache.kafka.common.Cluster;
+import org.apache.kafka.common.InvalidRecordException;
+import org.apache.kafka.common.PartitionInfo;
+
+import io.confluent.common.utils.Utils;
+
+public class BananaPartitioner implements Partitioner {
+
+  @Override
+  public void configure(Map<String, ?> configs) {}
+
+  @Override
+  public int partition(String topic, Object key, byte[] keyBytes,
+                       Object value, byte[] valueBytes, Cluster cluster) {
+    List<PartitionInfo> partitions = cluster.partitionsForTopic(topic);
+    int numPartitions = partitions.size();
+
+    if ((keyBytes == null) || (!(key instanceof String)))
+      throw new InvalidRecordException("We expect all messages to have customer name as key");
+
+    if (((String) key).equals("Banana"))
+      return numPartitions - 1;
+
+    return Math.abs(Utils.murmur2(keyBytes)) % (numPartitions - 1);
+  }
+
+  @Override
+  public void close() {}
+}
