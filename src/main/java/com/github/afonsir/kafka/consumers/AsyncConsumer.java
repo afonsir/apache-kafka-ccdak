a8bcc70feac1705474a5a93d884050ba78bd9ad1
AfonSir <afonsir17@gmail.com>
2025-12-31 12:49:23 -0300

    feat: add async consumer

---
+++ src/main/java/com/github/afonsir/kafka/consumers/AsyncConsumer.java
@@ -0,0 +1,69 @@
+package com.github.afonsir.kafka.consumers;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.consumer.OffsetCommitCallback;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AsyncConsumer {
+  private static final Logger log = LoggerFactory.getLogger(AsyncConsumer.class);
+
+  public static void main(String[] args) {
+    Properties props = new Properties();
+
+    props.put(
+      ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,
+      "localhost:29092,localhost:39092"
+    );
+
+    props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+    props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    props.put(ConsumerConfig.GROUP_ID_CONFIG, "AsyncCountryCounter");
+
+    props.put(
+      ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+      StringDeserializer.class.getName()
+    );
+
+    props.put(
+      ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+      StringDeserializer.class.getName()
+    );
+
+    try (Consumer<String, String> consumer = new KafkaConsumer<>(props)) {
+      consumer.subscribe(Collections.singletonList("customer.countries"));
+
+      Duration timeout = Duration.ofMillis(100);
+
+      while (true) {
+        ConsumerRecords<String, String> records = consumer.poll(timeout);
+
+        for (ConsumerRecord<String, String> record : records) {
+          System.out.printf(
+            "topic = %s, partition = %d, offset = %d, customer = %s, country = %s\n",
+            record.topic(), record.partition(), record.offset(), record.key(), record.value()
+          );
+        }
+
+        consumer.commitAsync(new OffsetCommitCallback() {
+          public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception e) {
+            if (e != null)
+              log.error("Commit failed for offsets {}", offsets, e);
+          }
+        });
+      }
+    }
+  }
+}
